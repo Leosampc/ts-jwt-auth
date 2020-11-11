5e3265d5253322c3ea08cd6d252aff2e84570c4a
Leonardo Cruz <leonardosamp11@gmail.com>
2020-11-11 18:00:21 -0300

    added auth and user routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import AuthController from './app/controllers/AuthController';
+
+import authMiddleware from './app/middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/users', UserController.store);
+router.post('/auth', AuthController.authenticate);
+router.get('/user', authMiddleware, UserController.fetch);
 
 export default router;
